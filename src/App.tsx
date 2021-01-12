4b279f71d1353eb9559bf555642c804851ba7387
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-01-12 16:35:21 +0900

    function components in typescript basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 interface AppProps {
-  message: string;
+  message?: string;
 }
 
-const App = ({ message }: AppProps) => {
-  return <>{message}</>;
+const App: React.FC<AppProps> = ({ message }) => {
+  return (
+    <>
+      <h4>{message}</h4>
+    </>
+  );
+};
+
+App.defaultProps = {
+  message: 'Hello, defaultProps!',
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App message="Hello" />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
