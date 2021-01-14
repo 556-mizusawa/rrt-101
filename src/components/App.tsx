e01f0ed4a9f7aa677e658f8da73c26b5639f65d9
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-01-14 19:21:22 +0900

    implement event creation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useReducer, MouseEvent } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reducer from '../reducers';
 
 const App: React.FC<{}> = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState('');
+  const [body, setbody] = useState('');
+
+  const addEvent = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log({ title, body });
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body,
+    });
+
+    setTitle('');
+    setbody('');
+  };
+
+  console.log({ state });
+
   return (
     <div className="container-fluid">
       <h4 className="mt-3">イベント作成フォーム</h4>
@@ -10,16 +30,28 @@ const App: React.FC<{}> = () => {
           <label htmlFor="formEventTitle" className="mt-3">
             タイトル
           </label>
-          <input className="form-control" id="formEventTitle" />
+          <input
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
 
         <div className="form-group">
           <label htmlFor="formEventBody" className="mt-3">
             ボディー
           </label>
-          <textarea className="form-control" id="formEventBody" />
+          <textarea
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={(e) => setbody(e.target.value)}
+          />
         </div>
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>
+          イベントを作成する
+        </button>
         <button className="btn btn-danger ml-1">全てのイベントを削除</button>
       </form>
 
