5fe02eb2d267594434dae5dcdf6db364d6a7aca8
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-01-23 20:25:50 +0900

    implement header and create css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <header className="container-fluid">
+      <h4>React Hooks + Redux + TypeScript　イベント作成アプリケーション</h4>
+    </header>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
