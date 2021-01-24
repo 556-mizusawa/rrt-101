3a5d561875981d7dab9fc4c65e52e8b355ca92fd
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-01-24 17:11:04 +0900

    Fixed time display.

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,7 @@
-export const timeCurrentIso8601: () => string = () => new Date().toISOString();
+export const timeCurrentIso8601: () => string = () => {
+  return new Date(Date.now() + 1000 * 60 * 60 * 9)
+    .toISOString()
+    .replace(/-/g, '/')
+    .replace('T', ' ')
+    .slice(0, 'YYYY/MM/DD hh:mm'.length);
+};
